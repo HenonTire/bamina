e0370c4b7376c415380d3b1034d1e41e064dff8c
Henon <henontireso@gmail.com>
2025-08-20 17:37:46 +0300

    modify address model

--- api/models.py
+++ api/models.py
@@ -74,9 +74,6 @@ class OrderStatus(models.TextChoices):
     DELIVERED = 'delivered', 'Delivered'
     CANCELLED = 'cancelled', 'Cancelled'
 
-# class PaymentMethod(models.TextChoices):
-#     CASH = 'cash', 'Cash on Delivery'
-#     PAY_HERE = 'pay_here', 'Card Payment'
 
 class Adress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -93,7 +90,7 @@ class Order(models.Model):
     total = models.DecimalField(decimal_places=2, max_digits=10)
     status = models.CharField(max_length=100, choices=OrderStatus.choices, default=OrderStatus.PENDING)
     shipping_address = models.OneToOneField(Adress, on_delete=models.CASCADE, blank=True)
-    # payment_method = models.CharField(max_length=50, choices=PaymentMethod.choices, default=PaymentMethod.CASH)
+
 
 
 class OrderItem(models.Model):
