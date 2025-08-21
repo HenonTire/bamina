45a237bbf102b9f4d197f6601c30122836c8d17c
Henon <henontireso@gmail.com>
2025-08-21 21:45:48 +0300

    very my last

--- bamina/storage/storage_backends.py
+++ bamina/storage/storage_backends.py
@@ -1,6 +1,6 @@
-# storage/storage_backends.py
+import os
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class SupabaseMediaStorage(S3Boto3Storage):
-    bucket_name = "media"
-    custom_domain = f"{bucket_name}.supabase.co/storage/v1/object/public"
+    bucket_name = os.getenv("AWS_STORAGE_BUCKET_NAME")  # reads from .env
+    custom_domain = f"{os.getenv('AWS_S3_ENDPOINT_URL')}/{bucket_name}/object/public"
