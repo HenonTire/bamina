5c874d59d7e208852d6ee22bf337219002995013
Henon <henontireso@gmail.com>
2026-09-14 15:59:56 +0300

    add sellerprofile into admin dashboard

--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, SellerProfile
 admin.site.register(User)
+admin.site.site_header = "Bamina Admin"
+admin.site.register(SellerProfile)
+
 # Register your models here.
